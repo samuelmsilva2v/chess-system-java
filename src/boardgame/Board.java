b58c23c18c9011afc083fbe357906d4cede5aa36
Samuel Maciel <samuelmsilva@outlook.com.br>
2023-06-21 21:26:59 -0300

    Handling exceptions and clearing screen

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 
 	public Board(int rows, int columns) {
 		if(rows < 1 || columns < 1) {
-			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column.");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -25,7 +25,7 @@ public class Board {
 	
 	public Piece piece(int row, int column) {
 		if(!positionExists(row, column)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board.");
 		}
 		return pieces[row][column];
 	}
